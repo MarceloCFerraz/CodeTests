147ed691d95e4f2f381f7cf7c7793cd5043cfaa8
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-03-28 10:54:26 -0300

    v1

--- LeetCode/ContainsDuplicate/Program.cs
+++ LeetCode/ContainsDuplicate/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Data;
+
+bool ContainsDuplicate(List<short> nums)
+{
+    if (nums.Count() < 1 || nums.Count() > 105)
+        throw new DataException("The array must contain min 1 and max 105 elements");
+    var read = new HashSet<short>();
+
+    for (int i = 0; i < nums.Count(); i++)
+    {
+        var num = nums[i];
+
+        if (read.Contains(num)) { return true; }
+        else { read.Add(num); }
+    }
+    return false;
+}
+
+Console.WriteLine($"Contains Duplis? {ContainsDuplicate(new List<short>(106) { 1, 2, 2, 4, 5 })}");
