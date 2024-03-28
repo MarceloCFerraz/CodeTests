e35e49175e4cce6ba942ce3f71e2faea0bae6418
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-03-28 11:33:21 -0300

    v1.2

--- LeetCode/ContainsDuplicate/Program.cs
+++ LeetCode/ContainsDuplicate/Program.cs
@@ -1,29 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
 
-bool ContainsDuplicate(List<short> nums)
+bool ContainsDuplicate(List<int> nums)
 {
-    if (nums.Count() < 1 || nums.Count() > 105)
-        throw new DataException("The array must contain min 1 and max 105 elements");
-    var read = new HashSet<short>();
+    var read = new HashSet<int>();
 
     for (int i = 0; i < nums.Count(); i++)
     {
-        var num = nums[i];
-
-        if (read.Contains(num)) { return true; }
-        else { read.Add(num); }
+        if (read.Contains(nums[i])) return true;
+        else read.Add(nums[i]);
     }
     return false;
 }
 
-var test1 = new List<short>(106) { 1, 2, 3, 1 };
+var test1 = new List<int>(106) { 1, 2, 3, 1 };
 var result1 = ContainsDuplicate(test1) ? "PASS" : "FAIL";
 
-var test2 = new List<short>(106) { 1, 2, 3, 4 };
+var test2 = new List<int>(106) { 1, 2, 3, 4 };
 var result2 = ContainsDuplicate(nums: test2) ? "FAIL" : "PASS";
 
-var test3 = new List<short>(106) { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+var test3 = new List<int>(106) { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
 var result3 = ContainsDuplicate(test3) ? "PASS" : "FAIL";
 
 Console.WriteLine($"Test 1: {result1}");
