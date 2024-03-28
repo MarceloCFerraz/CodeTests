712fb6cd733110e60013389e0e39fb9f72b72356
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-03-28 11:33:53 -0300

    v1.3

--- LeetCode/ContainsDuplicate/Program.cs
+++ LeetCode/ContainsDuplicate/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-bool ContainsDuplicate(List<int> nums)
+﻿bool ContainsDuplicate(List<int> nums)
 {
     var read = new HashSet<int>();
 
