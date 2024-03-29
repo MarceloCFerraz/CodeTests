16ba649ba6afffc592774e94b56ade4af0981684
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-03-29 12:23:27 -0300

    v2

--- LeetCode/TwoSum/Csharp/Program.cs
+++ LeetCode/TwoSum/Csharp/Program.cs
@@ -1,19 +1,17 @@
-﻿static int[] TwoSum(int[] nums, int target)
+﻿static int[]? TwoSum(int[] nums, int target)
 {
-    var response = new int[2];
+    var response = new Dictionary<int, int>();
 
     for (int i = 0; i < nums.Length; i++)
     {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[j] + nums[i] == target)
-            {
-                response = [i, j];
-                return response;
-            }
-        }
+        var difference = target - nums[i];
+
+        if (response.ContainsKey(difference))
+            return new int[] { i, response[difference] };
+
+        response[nums[i]] = i;
     }
-    return response;
+    return null;
 }
 
 /*
@@ -37,28 +35,28 @@ Example 3:
 
 
 var result1 = TwoSum([3, 2, 4], 6);
-var test1 = (
-    result1[0] == 1 && result1[1] == 2
-    || result1[0] == 2 && result1[1] == 1
-) && (result1.Length == 2)
+var test1 = result1 != null
+    && (result1[0] == 1 && result1[1] == 2
+        || result1[0] == 2 && result1[1] == 1)
+    && (result1.Length == 2)
     ? "PASS" : "FAIL";
 
 Console.WriteLine($"Test 1: {test1}");
 
 var result2 = TwoSum([3, 3], 6);
-var test2 = (
-    result2[0] == 0 && result2[1] == 1
-    || result2[0] == 1 && result2[1] == 0
-) && (result2.Length == 2)
+var test2 = result2 != null
+    && (result2[0] == 0 && result2[1] == 1
+        || result2[0] == 1 && result2[1] == 0)
+    && (result2.Length == 2)
     ? "PASS" : "FAIL";
 
 Console.WriteLine($"Test 2: {test2}");
 
 var result3 = TwoSum([2, 7, 11, 15], 9);
-var test3 = (
-    result3[0] == 0 && result3[1] == 1
-    || result3[0] == 1 && result3[1] == 0
-) && (result3.Length == 2)
+var test3 = result3 != null
+    && (result3[0] == 0 && result3[1] == 1
+        || result3[0] == 1 && result3[1] == 0)
+    && (result3.Length == 2)
     ? "PASS" : "FAIL";
 
 Console.WriteLine($"Test 3: {test3}");
