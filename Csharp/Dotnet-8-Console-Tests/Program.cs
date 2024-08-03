4b987f4da968429c19a4c2acdedeb96fbe0207e6
Marcelo Ferraz <marcelo.ferraz1.ext@bayer.com>
2024-08-03 10:35:35 -0300

    add code to get ComponentModel Description from an Enum

--- Csharp/Dotnet-8-Console-Tests/Program.cs
+++ Csharp/Dotnet-8-Console-Tests/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+public enum Colors
+{
+    [Description("a red color")]
+    Red = 1,
+    [Description("a green color")]
+    Green = 2,
+    [Description("a blue color")]
+    Blue = 3
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        foreach (var color in Enum.GetValues(typeof(Colors)))
+        {
+            var description = GetEnumDescription((Colors)color);
+            Console.WriteLine($"Color: {color}, id: {(int)color}, Description: {description}");
+        }
+    }
+
+    public static string GetEnumDescription(Colors color)
+    {
+        var field = color.GetType().GetField(color.ToString());
+
+        if (field == null) return "";
+
+        var attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        if (attributes != null && attributes.Length > 0)
+        {
+            return attributes[0].Description;
+        }
+        else
+        {
+            return color.ToString();
+        }
+    }
+}
