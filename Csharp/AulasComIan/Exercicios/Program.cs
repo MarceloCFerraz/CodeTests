7199ef90318a787a75912d624d3932ee451cae45
Marcelo Ferraz <marcelo.ferraz1.ext@bayer.com>
2024-10-05 19:29:45 -0300

    Update program to ask user to pick a question to test

--- Csharp/AulasComIan/Exercicios/Program.cs
+++ Csharp/AulasComIan/Exercicios/Program.cs
@@ -1,15 +1,69 @@
-﻿using Exercicios.Implements;
+﻿using System.Net.Quic;
+using Exercicios.Implements;
+using Exercicios.Interfaces;
 
-namespace Exercicios;
 
-class Program
-{
-    public static void Main(string[] args)
+var questions = new List<ICodingExercise>()
     {
-        var q1 = new ECondicionaisQ1();
-        var q2 = new ECondicionaisQ2();
+        new ECondicionaisQ1(),
+        new ECondicionaisQ2(),
+        new ECondicionaisQ3(),
+    };
 
-        q1.TestCode();
-        q2.TestCode();
+// infinite loop
+while (true)
+{
+    try
+    {
+        Console.Clear(); // clear previous messages on the terminal
+        var question = PickAnOption(questions);
+
+        if (question == null)
+        {
+            Console.WriteLine("You chose to leave, bye bye");
+            Environment.Exit(0);
+        }
+
+        Console.Clear(); // clear again to make test easier
+
+        question.TestCode();
+
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{ex.Message}. Please try again.");
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+    }
+}
+
+ICodingExercise? PickAnOption(List<ICodingExercise> questions)
+{
+    var higherOption = questions.Count;
+
+    Console.WriteLine($"Which question would you like to test? Type a number from 1 to {higherOption} or 0 to quit");
+    var input = Console.ReadLine();
+
+    ValidateInput(input, higherOption);
+
+    var questionIndex = int.Parse(input);
+
+    if (questionIndex == 0)
+        return null; // quits the program
+
+    return questions[questionIndex - 1];
+}
+
+static void ValidateInput(string? input, int higherOption)
+{
+    if (string.IsNullOrEmpty(input))
+        throw new InvalidDataException("Input can't be null");
+
+    if (!int.TryParse(input, out var questionIndex))
+        throw new InvalidCastException($"Can't convert '{input}' to an int");
+
+    if (questionIndex < 0 || questionIndex > higherOption)
+        throw new InvalidDataException($"You must choose an option between 0 and {higherOption}");
 }
